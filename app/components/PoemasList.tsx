4d5f64056209c3d5fd24d086d148de28bd12dbb2
TakashiNishii <takashi@geeklys.com>
2024-06-11 14:42:44 -0400

    :art: Zoom function

--- app/components/PoemasList.tsx
+++ app/components/PoemasList.tsx
@@ -5,17 +5,32 @@ import PoemasListCarousel from "./PoemasListCarousel"
 import { TypeView } from "../enum/TypeView"
 import TypeViewButton from "./TypeViewButton"
 import PoemasListGrid from "./PoemasListGrid"
+import classNames from "classnames"
+import Image from "next/image"
 
 const PoemasList = () => {
   const [typeView, setTypeView] = useState(TypeView.carousel) // ['carousel', 'grid']
+  const [imageSelected, setImageSelected] = useState(0)
+  const [open, setOpen] = useState(false)
   return (
     <>
       <TypeViewButton typeView={typeView} setTypeView={setTypeView} />
       {typeView === TypeView.carousel ?
         <PoemasListCarousel />
         :
-        <PoemasListGrid />
+        <PoemasListGrid setOpen={setOpen} setSelectedImage={setImageSelected} />
       }
+      <dialog id="my_modal_1" className={classNames("modal",
+        open && "modal-open"
+      )}>
+        <div className="modal-box flex justify-center">
+          <button className="btn btn-sm btn-circle btn-primary absolute right-2 top-2" onClick={() => setOpen(false)}>✕</button>
+          <Image width={400} height={400} src={`/Poemas/Poema${imageSelected}.jpeg`} alt='Poema visual' className="w-full" />
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button onClick={() => setOpen(false)}>close</button>
+        </form>
+      </dialog>
     </>
 
   )
--- app/components/PoemasListGrid.tsx
+++ app/components/PoemasListGrid.tsx
@@ -1,13 +1,21 @@
 import { MagnifyingGlassCircleIcon } from '@heroicons/react/16/solid'
 import Image from 'next/image'
 
-const PoemasListGrid = () => {
+interface PoemasListGridProps {
+  setOpen: (open: boolean) => void,
+  setSelectedImage: (image: number) => void
+}
+
+const PoemasListGrid = ({ setOpen, setSelectedImage }: PoemasListGridProps) => {
   return (
     <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-6'>
       {Array.from({ length: 14 }).map((_, i) => (
         <div key={i} id={`item${i + 1}`} className="border-2 border-secondary rounded-md flex flex-col justify-between items-center gap-2 ">
           <Image width={250} height={250} src={`/Poemas/Poema${i + 1}.jpeg`} alt='Poema visual' className="max-w-full rounded-t-sm flex-1" />
-          <MagnifyingGlassCircleIcon className="w-10 h-10 text-primary" />
+          <MagnifyingGlassCircleIcon className="w-10 h-10 text-primary" onClick={() => {
+            setSelectedImage(i + 1)
+            setOpen(true)
+          }} />
         </div>
       ))}
     </div>
